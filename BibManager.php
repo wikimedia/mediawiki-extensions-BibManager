8ec6f5f2a5d54756ef942bc421f7dcf99c0cba16
Jayprakash12345 <0freerunning@gmail.com>
2018-01-21 23:23:29 +0530

    Convert BibManager to use extension registation
    Bug: T185424
    Change-Id: I7cba88922da1b472ca4874eb356c9ff8a7fb1028

--- BibManager.php
+++ BibManager.php
@@ -18,115 +18,16 @@
   http://www.gnu.org/copyleft/gpl.html
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo 'To install BibManager, put the following line in LocalSettings.php: '
-	.'include_once( "$IP/extensions/BibManager/BibManager.php" );' . "\n";
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'BibManager' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['BibManager'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the BibManager extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the BibManager extension requires MediaWiki 1.29+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array (
-	'path'           => __FILE__,
-	'name'           => 'BibManager',
-	'author'         => array (
-		'Hornemann Institut', 'Hallo Welt! Medienwerkstatt GmbH',
-		'Tobias Weichart', 'Robert Vogel'
-	),
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:BibManager',
-	'version'        => '1.23.0',
-	'descriptionmsg' => 'bibmanager-desc',
-);
-
-//Register classes
-$wgAutoloadClasses['Structures_BibTex'] = __DIR__ . '/includes/libs/Structures_BibTex/BibTex.php'; //External library for parsing
-
-$wgAutoloadClasses['BibManagerHooks']               = __DIR__ . '/includes/BibManagerHooks.php';
-$wgAutoloadClasses['BibManagerFieldsList']          = __DIR__ . '/includes/BibManagerFieldsList.php';
-$wgAutoloadClasses['BibManagerLocalMWDatabaseRepo'] = __DIR__ . '/includes/BibManagerLocalMWDatabaseRepo.php';
-$wgAutoloadClasses['BibManagerPagerList']           = __DIR__ . '/includes/BibManagerPagerList.php';
-$wgAutoloadClasses['BibManagerPagerListAuthors']    = __DIR__ . '/includes/BibManagerPagerListAuthors.php';
-$wgAutoloadClasses['BibManagerRepository']          = __DIR__ . '/includes/BibManagerRepository.php';
-$wgAutoloadClasses['BibManagerValidator']           = __DIR__ . '/includes/BibManagerValidator.php';
-$wgAutoloadClasses['BibManagerLocalMWDatabaseRepo'] = __DIR__ . '/includes/BibManagerLocalMWDatabaseRepo.php';
-$wgAutoloadClasses['BibManagerRepository']          = __DIR__ . '/includes/BibManagerRepository.php';
-
-//SpecialPages
-$wgAutoloadClasses['SpecialBibManagerList']        = __DIR__ . '/includes/specials/SpecialBibManagerList.php';
-$wgAutoloadClasses['SpecialBibManagerListAuthors'] = __DIR__ . '/includes/specials/SpecialBibManagerListAuthors.php';
-$wgAutoloadClasses['SpecialBibManagerImport']      = __DIR__ . '/includes/specials/SpecialBibManagerImport.php';
-$wgAutoloadClasses['SpecialBibManagerExport']      = __DIR__ . '/includes/specials/SpecialBibManagerExport.php';
-$wgAutoloadClasses['SpecialBibManagerCreate']      = __DIR__ . '/includes/specials/SpecialBibManagerCreate.php';
-$wgAutoloadClasses['SpecialBibManagerDelete']      = __DIR__ . '/includes/specials/SpecialBibManagerDelete.php';
-$wgAutoloadClasses['SpecialBibManagerEdit']        = __DIR__ . '/includes/specials/SpecialBibManagerEdit.php';
-
-//Add I18N
-$wgMessagesDirs['BibManager'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['BibManagerAlias']  = __DIR__ . '/BibManager.alias.php';
-
-//Add SpecialPages
-$wgSpecialPages['BibManagerList']        = 'SpecialBibManagerList';
-$wgSpecialPages['BibManagerListAuthors'] = 'SpecialBibManagerListAuthors';
-$wgSpecialPages['BibManagerImport']      = 'SpecialBibManagerImport';
-$wgSpecialPages['BibManagerExport']      = 'SpecialBibManagerExport';
-$wgSpecialPages['BibManagerDelete']      = 'SpecialBibManagerDelete';
-$wgSpecialPages['BibManagerCreate']      = 'SpecialBibManagerCreate';
-$wgSpecialPages['BibManagerEdit']        = 'SpecialBibManagerEdit';
-
-//Add Hookhandler
-$wgHooks['ParserFirstCallInit'][] = 'BibManagerHooks::onParserFirstCallInit';
-$wgHooks['BeforePageDisplay'][]   = 'BibManagerHooks::onBeforePageDisplay';
-//$wgHooks['SkinAfterContent'][]    = 'BibManagerHooks::onSkinAfterContent';
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'BibManagerHooks::onLoadExtensionSchemaUpdates';
-
-$resourceModuleTemplate = array(
-	'localBasePath' => __DIR__.'/resources',
-	'remoteExtPath' => 'BibManager/resources'
-);
-
-$wgResourceModules['ext.bibManager.styles'] = array(
-	'styles' => 'ext.bibManager.css',
-) + $resourceModuleTemplate;
-
-$wgResourceModules['ext.bibManager.List'] = array(
-	'scripts' => 'ext.bibManager.List.js',
-	'dependencies' => 'ext.bibManager.styles'
-) + $resourceModuleTemplate;
-
-$wgResourceModules['ext.bibManager.Edit'] = array(
-	'scripts' => 'ext.bibManager.Edit.js',
-	'dependencies' => 'ext.bibManager.styles'
-) + $resourceModuleTemplate;
-
-unset( $resourceModuleTemplate );
-
-//Config Variables
-$wgBibManagerRepoClass = 'BibManagerLocalMWDatabaseRepo';
-$wgBibManagerCitationFormats = array (
-    '-'             => '%author%: %title%, %year%',
-    'article'       => '%author% (%year%): %title%. <em>%journal%</em>, %volume%, %pages%',
-    'book'          => '%author% (%year%): %title%. (%edition%). <em>%publisher%</em>, %address%, %pages%',
-    'booklet'       => '%title%',
-    'conference'    => '%author% (%year%): %title%. %booktitle%',
-    'inbook'        => '%author% (%year%): %title%. (%edition%). <em>%publisher%</em>, %address%, %pages%, %editor%, %chapter%',
-    'incollection'  => '%author% (%year%): %title%.  %booktitle%',
-    'inproceedings' => '%author% (%year%): %title%. <em>%publisher%</em>, %booktitle%',
-    'manual'        => '%title%',
-    'mastersthesis' => '%author% (%year%): %title%. %school%',
-    'misc'          => '%author%: %title%, %year%',
-    'phdthesis'     => '%author% (%year%): %title%. %school%',
-    'proceedings'   => '%title% (%year%)',
-    'techreport'    => '%author% (%year%): %title%. %institution%.',
-    'unpublished'   => '%author%: %title%. %note%.'
-);
-
-//$wgBibManagerSkinAfterContentBibListConfig = array( 'hr' => true, 'hideonemptylist' => true ); //For future use.
-$wgBibManagerCitationArticleNamespace = NS_MAIN;
-$wgBibManagerScholarLink = 'http://scholar.google.com/scholar?q=%title%';
-
-// Basic permissions
-$wgGroupPermissions['sysop']['bibmanageredit']   = true;
-$wgGroupPermissions['sysop']['bibmanagerdelete'] = true;
-$wgGroupPermissions['sysop']['bibmanagercreate'] = true;
-
-$wgAvailableRights[] = 'bibmanageredit';
-$wgAvailableRights[] = 'bibmanagerdelete';
-$wgAvailableRights[] = 'bibmanagercreate';
